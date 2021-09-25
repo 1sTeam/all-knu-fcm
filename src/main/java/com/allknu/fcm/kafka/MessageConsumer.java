36b0b63920f3a930c88dd4bbf5192063f84985f7
suhongkim98 <kshz9815@naver.com>
2021-09-25 18:20:13 +0900

    카프카로부터 받은 메시지 푸시 알림 구현

--- src/main/java/com/allknu/fcm/kafka/MessageConsumer.java
+++ src/main/java/com/allknu/fcm/kafka/MessageConsumer.java
@@ -1,6 +1,9 @@
 package com.allknu.fcm.kafka;
 
 import com.allknu.fcm.kafka.dto.FCMWebMessage;
+import com.allknu.fcm.utils.FCMUtil;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -10,10 +13,18 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class MessageConsumer {
+    private final FCMUtil fcmUtil;
 
     @KafkaListener(topics = "fcm", groupId = "all-knu-fcm", containerFactory = "fcmRequestMessageListener")
     public void consume(@Payload FCMWebMessage message, @Headers MessageHeaders headers) throws IOException {
         System.out.println(String.format("Consumed message : %s", message.getBody()));
+
+        try {
+            fcmUtil.sendFCMToTopics(message);
+        }catch (FirebaseMessagingException firebaseMessagingException) {
+            System.out.println(firebaseMessagingException);
+        }
     }
 }
