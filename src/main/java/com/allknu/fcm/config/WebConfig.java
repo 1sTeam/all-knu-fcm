807719cdfa533ce8569fc2158b33f64231752df5
suhongkim98 <kshz9815@naver.com>
2022-05-16 21:58:18 +0900

    feature: cors 설정

---
+++ src/main/java/com/allknu/fcm/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.allknu.fcm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") //cors 문제 해결
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
