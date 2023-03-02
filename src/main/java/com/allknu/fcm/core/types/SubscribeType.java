49d19573d0e99c4701037c30748758479dbaefd8
suhongkim98 <kshz9815@naver.com>
2023-03-02 23:09:01 +0900

    feature : 학사구조개편 반영

--- src/main/java/com/allknu/fcm/core/types/SubscribeType.java
+++ src/main/java/com/allknu/fcm/core/types/SubscribeType.java
@@ -1,6 +1,9 @@
 package com.allknu.fcm.core.types;
 import lombok.Getter;
 
+/**
+ * all-knu-backend의 SubscribeType과 동일해야한다.
+ */
 @Getter
 public enum SubscribeType {
 
@@ -13,9 +16,14 @@ public enum SubscribeType {
     GLOBAL("대외교류센터", "univ"), // 대외교류
     CCE("창의융합교육센터", "univ"),
     DEC("원격교육지원센터", "univ"),
+    JOB_PLUS("대학일자리플러스센터", "univ"),
+    GLOBAL_CONTRIBUTE("글로벌사회공헌센터","univ"),
+    DISABLED_SUPPORT("장애학생지원센터","univ"),
+    SOCIAL_ENTERPRISE("사회적기업지원센터", "univ"),
+    ROTC("ROTC", "univ"),
 
     //대학
-    IWC("ICT건설공과대학", "college"),
+    IWC("공과대학", "college"),
     BAM("경영관리대학", "college"),
     WC("복지융합대학", "college"),
     GT("글로벌인재대학", "college"),
@@ -26,13 +34,11 @@ public enum SubscribeType {
     SOFTWARE("ICT융합공학부", "major"), //소응
     WELFARE("사회복지학부", "major"),
     SENIOR("실버산업학과", "major"),
-    UVD("유니버셜디자인전공", "major"),
-    VISUAL_ART("미술문화복지전공", "major"),
-    SPORT("스포츠복지전공", "major"),
+    UVD("유니버셜아트디자인학과", "major"),
+    SPORT("스포츠복지학과", "major"),
     CHRISTIANITY("기독교학과", "major"),
     KOREA_CULTURE("한영문화콘텐츠학과", "major"),
-    INTERNATIONAL_REGIONAL("국제지역학전공", "major"),
-    CHINA_REGIONAL("중국지역학전공", "major"),
+    INTERNATIONAL_REGIONAL("글로벌문화학부", "major"),
     MUSIC("음악학과", "major"),
     EDU("교육학과", "major"),
     CHILDHOOD_EDU("유아교육과", "major"),
@@ -40,7 +46,6 @@ public enum SubscribeType {
     GLOBAL_BIZ("글로벌경영학부", "major"),
     PUBLIC("공공인재학과", "major"),
     TAX_ECONOMIC("정경학부", "major"),
-    IOT("IOT전자공학과", "major"),
     INTELLIGENCE("인공지능융합공학부", "major"),
     CONSTRUCTION("부동산건설학부", "major");
 
